4dcd9184071e012535c61f1709ec5a5a17c8102d
Faizila Amlani <amlani.faizila@gmail.com>
2021-10-26 17:42:02 -0500

    added more dependencies and comments in server.js

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+// Dependencies
 const express = require('express');
 const routes = require('./routes');
+const mysql = require('mysql2');
+require('dotenv').config();
+
 // import sequelize connection
 const sequelize = require('./config/connection');
 
+// Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middlewware to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
