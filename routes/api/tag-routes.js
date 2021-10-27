d5de2140c58306b3df1f4896461b938fd6797508
Faizila Amlani <amlani.faizila@gmail.com>
2021-10-27 18:51:54 -0500

    added more routes in route api folder

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,13 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const TagData = await Tag.findAll();
+    res.status(200).json(TagData);
+  } 
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
@@ -15,6 +22,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const TgData = await Tag.create(req.body);
+    res.status(200).json(TgData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +36,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const TagsData = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    if (!TagsData) {
+      res.status(404).json({ message: 'No Tag with this id!' });
+      return;
+    }
+    res.status(200).json(TagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
