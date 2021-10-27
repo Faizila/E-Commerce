d368c9f32ea80f77c130b0b57656bacd8d4aa488
Faizila Amlani <amlani.faizila@gmail.com>
2021-10-27 18:37:23 -0500

    added few routes in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,13 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const CategoryData = await Category.findAll();
+    res.status(200).json(CategoryData);
+  } 
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
@@ -15,6 +22,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const CtData = await Category.create(req.body);
+    res.status(200).json(CtData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +36,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const CtData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!CtData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(tripData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
